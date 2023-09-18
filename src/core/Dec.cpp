e300eaad6cfac2b131750c8a27b1156fa55be71d
Terlibin Oleg <rumblefishinfo@gmail.com>
2023-09-19 00:54:06 +0300

    Dec module

---
+++ src/core/Dec.cpp
@@ -0,0 +1,110 @@
+#include "Dec.h"
+#include "../config.h"
+#include <Math.h>
+
+
+void MotorController::initialize()
+{
+  setRpm(DEC_DEFAULT_RPM);
+  Serial.println("DEC MODULE: initialization finished"); 
+}
+
+void MotorController::update()
+{
+  if (parking)
+  {
+    return;
+  }
+
+  bool changeSide = currentSide != side;
+
+  if (changeSide)
+  {
+    Serial.println("DEC MODULE: change side");
+
+    long stepsToDefaultPosition = getStepsToMove(
+      getDegreeSeconds(currentDegree, currentMinute, currentSeconds) - getDegreeSeconds(DEFAULT_DEC_DEGREE, DEFAULT_DEC_MINUTE, DEFAULT_DEC_SECONDS)
+    );
+    move(stepsToDefaultPosition * currentSide);
+
+    currentSide = side;
+    currentDegree = DEFAULT_DEC_DEGREE;
+    currentMinute = DEFAULT_DEC_MINUTE;
+    currentSeconds = DEFAULT_DEC_SECONDS;
+  }
+
+  long currentTotalSeconds = getDegreeSeconds(currentDegree, currentMinute, currentSeconds);
+  long totalSeconds = getDegreeSeconds(degree, minute, seconds);
+
+  if (currentTotalSeconds != totalSeconds)
+  {
+    Serial.println("DEC MODULE: change coordinate");
+    long secondsForMove = currentTotalSeconds - totalSeconds;
+    long steps = getStepsToMove(secondsForMove);
+    move(steps * currentSide);
+
+    currentDegree = degree;
+    currentMinute = minute;
+    currentSeconds = seconds;
+  }
+}
+
+void MotorController::setRpm(int rpm)
+{
+  decStepper.setSpeed(rpm);
+}
+
+void MotorController::setCoordinates(int deg, int min, int sec)
+{
+  degree = deg;
+  minute = min;
+  seconds = sec;
+}
+
+Coordinates MotorController::getCoordinates(Coordinates C)
+{
+  C.deg = currentDegree;
+  C.min = currentMinute;
+  C.sec = currentSeconds;
+
+  return (C);
+}
+
+void MotorController::setSide(int newSide)
+{
+  currentSide = newSide;
+}
+
+void MotorController::setParking(bool newState)
+{
+  parking = newState;
+}
+
+long MotorController::getDegreesInSeconds(int deg, int min, int sec)
+{
+  long seconds = ((((long)abs(deg) * 60) + min) * 60) + sec;
+  return (deg / abs(deg)) * seconds;
+}
+
+long MotorController::getStepsToMove(long secondsForMove)
+{
+  return secondsForMove / secondsPerStep;
+}
+
+void MotorController::move(long steps)
+{
+  Serial.println("DEC MODULE: move move...");
+  // Is the number of steps per command
+  int iterationSteps = 10000;
+  int index = abs(steps / iterationSteps);
+  int remainderSteps = steps % iterationSteps;
+
+  while (index > 0)
+  {
+    decStepper.step(iterationSteps);
+    index--;
+  }
+
+  decStepper.step(remainderSteps);
+  Serial.println("DEC MODULE: move over");
+}
--- src/core/Dec.h
+++ src/core/Dec.h
@@ -1,136 +1,55 @@
-#include <Stepper.h>
-#include <Math.h>
 #include "../config.h"
+#include <Stepper.h>
 
 Stepper decStepper(DEC_MOTOR_STEPS, DEC_DIRECTION_PIN, DEC_SPEED_PIN);
 
+struct Coordinates {
+  int deg;
+  int min;
+  int sec;
+};
+
+
 class Dec
 {
-public:
-  void initialize()
-  {
-    setRpm(DEC_DEFAULT_RPM);
-    Serial.println("DEC MODULE: initialization finished"); 
-  }
+  private:
+    bool parking = false;
+    
+    int currentDegree = DEFAULT_DEC_DEGREE;
+    int currentMinute = DEFAULT_DEC_MINUTE;
+    int currentSeconds = DEFAULT_DEC_SECONDS;
+    
+    // "1" - right side, "-1" - left side
+    int currentSide = 1;
+    int side = 1;
+    
+    int degree = DEFAULT_DEC_DEGREE;
+    int minute = DEFAULT_DEC_MINUTE;
+    int seconds = DEFAULT_DEC_SECONDS;
+    
+    double stepsPerFullTurn = DEC_MOUNT_STEPS * DEC_MOTOR_STEPS * DEC_GEAR_TRAIN * DEC_MICRO_STEPS;
+    double secondsForFullTurn = 360.00 * 60.00 * 60.00;
+    double secondsPerStep = secondsForFullTurn / stepsPerFullTurn;
 
-  void update()
-  {
-    if (parking)
-    {
-      return;
-    }
+    long getDegreesInSeconds(int deg, int min, int sec)
+  
+    long getStepsToMove(long secondsForMove)
 
-    bool changeSide = currentSide != side;
+    void move(long steps)
 
-    if (changeSide)
-    {
-      Serial.println("DEC AXIS: change side");
 
-      long stepsToDefaultPosition = getStepsToMove(
-        getDegreeSeconds(currentDegree, currentMinute, currentSeconds) - getDegreeSeconds(DEFAULT_DEC_DEGREE, DEFAULT_DEC_MINUTE, DEFAULT_DEC_SECONDS)
-      );
-      move(stepsToDefaultPosition * currentSide);
+  public:
+    void initialize()
 
-      currentSide = side;
-      currentDegree = DEFAULT_DEC_DEGREE;
-      currentMinute = DEFAULT_DEC_MINUTE;
-      currentSeconds = DEFAULT_DEC_SECONDS;
-    }
+    void update()
 
-    long currentTotalSeconds = getDegreeSeconds(currentDegree, currentMinute, currentSeconds);
-    long totalSeconds = getDegreeSeconds(degree, minute, seconds);
+    void setRpm(int rpm)
 
-    if (currentTotalSeconds != totalSeconds)
-    {
-      Serial.println("DEC AXIS: change coordinate");
-      long secondsForMove = currentTotalSeconds - totalSeconds;
-      long steps = getStepsToMove(secondsForMove);
-      move(steps * currentSide);
-
-      currentDegree = degree;
-      currentMinute = minute;
-      currentSeconds = seconds;
-    }
-  }
-
-  void setRpm(int rpm)
-  {
-    decStepper.setSpeed(rpm);
-  }
-
-  long setDegree(int deg, int min, int sec)
-  {
-    degree = deg;
-    minute = min;
-    seconds = sec;
-  }
-
-  int getDegree()
-  {
-    return currentDegree;
-  }
-
-  int getMinute()
-  {
-    return currentMinute;
-  }
-
-  int getSeconds()
-  {
-    return currentSeconds;
-  }
-
-  void setSide(int newSide)
-  {
-    currentSide = newSide;
-  }
-
-  void changeParkingState()
-  {
-    parking = !parking;
-  }
-
-private:
-  bool parking = false;
-  int currentDegree = DEFAULT_DEC_DEGREE;
-  int currentMinute = DEFAULT_DEC_MINUTE;
-  int currentSeconds = DEFAULT_DEC_SECONDS;
-  // "1" - right side, "-1" - left side
-  int currentSide = 1;
-  int side = 1;
-  int degree = DEFAULT_DEC_DEGREE;
-  int minute = DEFAULT_DEC_MINUTE;
-  int seconds = DEFAULT_DEC_SECONDS;
-  double stepsPerFullTurn = DEC_MOUNT_STEPS * DEC_MOTOR_STEPS * DEC_GEAR_TRAIN * DEC_MICRO_STEPS;
-  double secondsForFullTurn = 360.00 * 60.00 * 60.00;
-  double secondsPerStep = secondsForFullTurn / stepsPerFullTurn;
-
-  long getDegreeSeconds(int deg, int min, int sec)
-  {
-    long seconds = ((((long)abs(deg) * 60) + min) * 60) + sec;
-    return (deg / abs(deg)) * seconds;
-  }
-
-  long getStepsToMove(long secondsForMove)
-  {
-    return secondsForMove / secondsPerStep;
-  }
-
-  void move(long steps)
-  {
-    Serial.println("DEC AXIS: move move...");
-    // Is the number of steps per command
-    int iterationSteps = 10000;
-    int index = abs(steps / iterationSteps);
-    int remainderSteps = steps % iterationSteps;
-
-    while (index > 0)
-    {
-      decStepper.step(iterationSteps);
-      index--;
-    }
-
-    decStepper.step(remainderSteps);
-    Serial.println("DEC AXIS: move over");
-  }
+    void setCoordinates(int deg, int min, int sec)
+    
+    Coordinates getCoordinates(Coordinates C)
+  
+    void setSide(int newSide)
+  
+    void setParking(bool parking)
 };
